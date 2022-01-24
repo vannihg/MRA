5213346f4b7cb0afad25ea41bd631800e330ea0f
van nhi <93583228+vannihg@users.noreply.github.com>
2022-01-24 12:49:01 +0100

    Update MD_Adapter.java

--- src/dbadapter/MD_Adapter.java
+++ src/dbadapter/MD_Adapter.java
@@ -78,72 +78,47 @@ public class MD_Adapter implements IMovieDatabase {
 	
 	
 	
-	//Check if movie already exist
-		public boolean checkMovie(String title, int publishingDate) {
-
-			// Declare necessary SQL query.
-			String queryMD = "SELECT FROM mydb01.movie WHERE title=? AND publishingDate=?";
-
-			// query data.
+	public boolean addMovie(String title, String director, String actors, TimeData publishingDate, int mid) {
+		String sqlInsert = "INSERT INTO mydb.01.movie (title, director, actors, publishingDate, mid) VALUES (?,?,?,?,?)";
+		
+		Boolean notExist = check_movie(title, director, actors, publishingDate, mid);
+		if (notExist) {
 			try (Connection connection = DriverManager
-					.getConnection(
-							"jdbc:" + Configuration.getType() + "://" + Configuration.getServer() + ":"
-									+ Configuration.getPort() + "/" + Configuration.getDatabase(),
-							Configuration.getUser(), Configuration.getPassword())) {
-				try (PreparedStatement psSelect = connection.prepareStatement(queryMD)) {
-					psSelect.setString(1, title);
-					psSelect.setInt(2, publishingDate);
-					try (ResultSet rs = psSelect.executeQuery()) {
-						return rs.next();
-					}
+					.getConnection("jdbc:mysql://localhost:3306/mydb01")){
+				try (PreparedStatement ps = connection.prepareStatement(sqlInsert)) {
+					ps.setString(1, title);
+					ps.setString(2, director);
+					ps.setString(3, actors);
+					ps.setInt(4, publishingDate.getDay());
+					ps.setInt(5, publishingDate.getMonth());
+					ps.setInt(6, publishingDate.getYear());
+					ps.setInt(7, mid);
+					ps.executeUpdate();
 				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				return false;
+				
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
+			catch (Exception e) {
+				e.printStackTrace();
+			}
+				return true;
+			
+		} else {
+			return false;
 		}
 		
 		
 		
-		public boolean addMovie(String title, String director, String actors, TimeData publishingDate) {
-			 
-			String sqlInsertM = "INSERT INTO mydb01.movie (title, director, actors, publishingDate) VALUES (?,?,?,?)";
-	        
-			Boolean notExist=checkMovie(title, publishingDate);
-			if(notExist)
-			{
-			try (Connection connection = DriverManager
-					.getConnection("jdbc:mysql://localhost:3306/mydb01")) {
-	            
-				try (PreparedStatement ps = connection.prepareStatement(sqlInsertM)) {
-					ps.setString(1, title);
-					ps.setString(2, director);
-					ps.setString(3, actors);
-					ps.setString(4, publishingDate);
-					ps.executeUpdate();
-					 
-					
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-			  } catch (Exception e) {
-				  e.printStackTrace();}
-			  return true;
-			}else {
-			return false;}
-			 
 	}
-		
-		
-		public boolean checkMovie(String title, TimeData publishingDate) {
-			 if(checkMovie(title, publishingDate) == true)
-			 { return true;}
-			 else
-				 return false;
-			}
-
-		
-		
-		
 	
+	public boolean check_movie(String title, String director, String actors, TimeData publishingDate, int mid) {
+		if(publishingDate.getDay() == 0 & title == "") {
+			return true;
+			
+		}
+		else
+			return false;
+	}
 }
